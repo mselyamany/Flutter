05b6991adf39ac767102d40e7c5ff6ed9d6b010a
Mohamed Elyamany <169154707+mselyamany@users.noreply.github.com>
2025-02-20 17:09:44 +0200

    Update Task1.dart

--- Task1.dart
+++ Task1.dart
@@ -1,1 +1,27 @@
+void main() {
+  Map car = {
+    'Brand': 'BMW',
+    'Model': 'BMW 520',
+    'Color': 'black',
+    'Price': '2000000'
+  };
+  print(car);
+  if (car['Brand'] == 'BMW') {
+    print('Yes BMW');
+  } else if (car['Brand'] == 'Kia') {
+    print('No BMW');
+  } else if (car['Brand'] == 'Toyota') {
+    print('No BMW');
+  } else {
+    print('No Error');
+  }
+
+  switch (car['Color']) {
+    case 'black':
+      print('Yes the Color black');
+      break;
+    case 'red':
+      print('No the Color black');
+  }
+}
 
