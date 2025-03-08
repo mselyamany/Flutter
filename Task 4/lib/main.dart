5972b852804d102cb89c2466f39bdadb2bbf8bd0
Mohamed Elyamany <169154707+mselyamany@users.noreply.github.com>
2025-03-08 03:14:28 +0200

    Update main.dart

--- Task 4/lib/main.dart
+++ Task 4/lib/main.dart
@@ -4,4 +4,15 @@ void main() {
   BankAccount deposit1 = BankAccount();
   deposit1.setBalance = 100000;
   print(deposit1.getBalance * 2);
+
+  Dogs dog1 = Dogs();
+  dog1.makeSound();
+  Cats cat1 = Cats();
+  cat1.makeSound();
+
+  Rectangle r1 = Rectangle();
+  r1.length = 12;
+  r1.width = 6;
+  r1.sumArea();
+  r1.sumPerimeter();
 }
